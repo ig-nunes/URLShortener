a7da703d116af90993835b4b70c23d25ac6b1257
Igor Nunes <inunes.us@gmail.com>
2024-03-02 03:51:11 -0300

    feat: add custom exception

---
+++ URLShortener.Dados/CustomExceptions/InvalidUrlException .cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace URLShortener.Dados.CustomExceptions
+{
+    public class InvalidUrlException : Exception
+    {
+        public int StatusCode { get; }
+        public InvalidUrlException(string message, int statusCode) : base(message)
+        {
+            StatusCode = statusCode;
+        }
+    }
+}
