c506c2edbbebf2f490ed7b60b00db99b163ce7cd
Igor Nunes <inunes.us@gmail.com>
2024-03-01 22:05:04 -0300

    feat: add migration initial-migration

---
+++ URLShortener.Dados/Migrations/20240301160610_initial-migration.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace URLShortener.Dados.Migrations
+{
+    /// <inheritdoc />
+    public partial class initialmigration : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Urls",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "INTEGER", nullable: false)
+                        .Annotation("Sqlite:Autoincrement", true),
+                    LUrl = table.Column<string>(type: "TEXT", nullable: false),
+                    SUrl = table.Column<string>(type: "TEXT", nullable: false),
+                    CreationDate = table.Column<DateTime>(type: "TEXT", nullable: false),
+                    ExpirationPeriod = table.Column<int>(type: "INTEGER", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Urls", x => x.Id);
+                });
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Urls");
+        }
+    }
+}
--- URLShortener.Dados/UrlShortenerContext.cs
+++ URLShortener.Dados/UrlShortenerContext.cs
@@ -1,12 +1,21 @@
-﻿using System;
+﻿using Microsoft.EntityFrameworkCore;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using URLShortener.Dados.Models;
 
 namespace URLShortener.Dados
 {
-    public class UrlShortenerContext
+    public class UrlShortenerContext : DbContext
     {
+        public UrlShortenerContext(DbContextOptions options) : base(options) 
+        { 
+        
+        }
+        
+        public virtual DbSet<Url> Urls { get; set; }
+
     }
 }
